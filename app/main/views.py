0505d840d6e5c235aa4cb7848ea8c6923242b45d
kazugufaith <faithmugesia@gmail.com>
2019-05-20 19:23:38 +0300

    added a decorator for entertainment on views.py file

--- app/main/views.py
+++ app/main/views.py
@@ -24,40 +24,40 @@ def sport():
 	title = 'general-news Page - Get The latest News Online'
 	return render_template('sports.html',title = title,sports=general_news)
 
-# @main.route('/technology')
-# def tech():
-# 	'''
-# 	View root page function that returns the index page and its data
-# 	'''
+@main.route('/technology')
+def tech():
+	'''
+	View root page function that returns the index page and its data
+	'''
 
-# 	general_news = get_news('technology')
-# 	title = 'general-news Page - Get The latest News Online'
-# 	return render_template('technology.html',title = title,technology=general_news)
+	general_news = get_news('technology')
+	title = 'general-news Page - Get The latest News Online'
+	return render_template('technology.html',title = title,technology=general_news)
 
-# @main.route('/businessNews')
-# def business():
-# 	'''
-# 	View root page function that returns the index page and its data
-# 	'''
+@main.route('/businessNews')
+def business():
+	'''
+	View root page function that returns the index page and its data
+	'''
 
-# 	general_news = get_news('business')
-# 	title = 'general-news Page - Get The latest News Online'
-# 	return render_template('businessNews.html',title = title,businessNews=general_news)
+	general_news = get_news('business')
+	title = 'general-news Page - Get The latest News Online'
+	return render_template('businessNews.html',title = title,businessNews=general_news)
 
-# @main.route('/entertainmentNews')
-# def entertainment():
-# 	'''
-# 	View root page function that returns the index page and its data
-# 	'''
-# 	# Getting popular news
-# 	general_news = get_news('entertainment')
-# 	title = 'general-news Page - Get The latest News Online'
-# 	return render_template('entertainmentNews.html',title = title,entertainment=general_news)
+@main.route('/entertainmentNews')
+def entertainment():
+	'''
+	View root page function that returns the index page and its data
+	'''
+	# Getting popular news
+	general_news = get_news('entertainment')
+	title = 'general-news Page - Get The latest News Online'
+	return render_template('entertainmentNews.html',title = title,entertainment=general_news)
 
 
-# @main.route('/')
-# def index():
+@main.route('/')
+def index():
 
-#     news_article = get_article('bitcoin')
-#     title = 'Home Page - Get The latest News Online'
-#     return render_template('index.html',title = title, article=news_article)
+    news_article = get_article('bitcoin')
+    title = 'Home Page - Get The latest News Online'
+    return render_template('index.html',title = title, article=news_article)
